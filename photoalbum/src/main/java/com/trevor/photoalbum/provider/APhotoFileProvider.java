db037ab64a89465359acc318a94123c6ca66026a
DAIFEI <407584230@qq.com>
2018-03-09 17:10:57 +0800

    修复错误

---
+++ photoalbum/src/main/java/com/trevor/photoalbum/provider/APhotoFileProvider.java
@@ -0,0 +1,12 @@
+package com.trevor.photoalbum.provider;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ *
+ * @author DAIFEI
+ * @date 2018/3/9
+ */
+public class APhotoFileProvider extends FileProvider {
+
+}
